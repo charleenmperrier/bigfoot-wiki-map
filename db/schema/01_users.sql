6b2b5e3b44b08c5f80846135913bd7c421d1730a
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-07 00:07:00 +0000

    added sql table files

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,9 @@
--- Drop and recreate Users table (Example)
+-- Drop and recreate Users table
 
 DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(255) NOT NULL,
+favourite_id INTEGER REFERENCES favourites(id) ON DELETE CASCADE
 );

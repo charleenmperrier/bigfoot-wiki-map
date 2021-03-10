6c4f0cc2cbe56f063c43f8d73bf51db8e0e641ab
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-10 18:17:53 +0000

    added login query

--- routes/login.js
+++ routes/login.js
@@ -6,13 +6,29 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     const username = req.session.user_id;
     const templateVars = {username}
+
+
     res.render("logged-in", templateVars);
   });
   router.post("/", (req, res) => {
     console.log('req.body: ', req.body)
-      req.session.user_id = req.body.username;
-    res.redirect('/maps')
+    const username = req.body.username;
+    console.log('username: ', username);
+    db.query(`SELECT * FROM users
+    WHERE name = 'Bob';`)
+
+    .then(data => {
+          req.session.user_id = username
+          const templateVars = {username}
+          res.render('logged-in', templateVars);
+        })
+    .catch(err => {
+      res
+          .status(403)
+          .send('error 403: username not found');
+      });
+
   });
   return router;
 };
-
+// %${username}%

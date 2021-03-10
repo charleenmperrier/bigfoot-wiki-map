226f445af6a1219c6e5401329183d0d728dc9dba
Molly Trepanier <mtrepanier123@gmail.com>
2021-03-10 05:53:48 +0000

    fixed templating issue with ejs

--- server.js
+++ server.js
@@ -64,7 +64,9 @@ app.get("/", (req, res) => {
   res.redirect("maps");
 });
 app.get("/index", (req, res) => {
-  res.render('index')
+  const username = req.session.user_id;
+    const templateVars = {username}
+  res.render('index', templateVars)
 })
 
 // app.get("/login", (req,res) => {
@@ -89,5 +91,5 @@ app.get("/logout", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Bigfoot is watching you from ${PORT}! 👀`);
 });

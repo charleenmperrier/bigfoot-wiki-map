a2dd41a97b92d3f26dbe05a9d6c6d9716462d292
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-10 03:11:42 +0000

    refactor/ adding styles

--- server.js
+++ server.js
@@ -56,8 +56,6 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-
-
   res.render("index");
 });
 

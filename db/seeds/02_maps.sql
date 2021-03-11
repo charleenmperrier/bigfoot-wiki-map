6a8cb57adb851a163be2e4727ff31f2070f35380
Molly Trepanier <mtrepanier123@gmail.com>
2021-03-11 04:52:29 +0000

    added bigfoot stock photos tos to project, added photo value to pin data

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -2,4 +2,4 @@
 
 INSERT INTO maps (name, lon, lat, user_id)
 VALUES ('Lower Mainland & Vancouver Island', 50, -123.100, 1),
-('Ottawa', 45.4239, -75.7009, 1), ('St. Augustine, Florida', 29.84, -81.23, 2);
+('Winnipeg', 49.8951, -97.1384, 1), ('St. Augustine, Florida', 29.84, -81.23, 2);

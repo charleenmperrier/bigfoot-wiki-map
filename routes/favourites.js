9b95f6f367ab6345f7598d3b3a6fdcdad36cafcd
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-11 18:29:21 +0000

    fav button and delete button

--- routes/favourites.js
+++ routes/favourites.js
@@ -6,7 +6,7 @@ module.exports = (db) => {
 
   router.get("/", (req, res) => {
     const username = req.session.user_id;
-    console.log('fav user: ', username)
+    // console.log('fav user: ', username)
     db.query(`
     SELECT map_id, maps.name
     FROM favourites
@@ -14,14 +14,11 @@ module.exports = (db) => {
     JOIN maps ON maps.id = map_id
     WHERE users.name LIKE '%${username}%';`)
     .then(data => {
-      // const favs = data.rows;
       const username = req.session.user_id
           const templateVars = {
             username,
             favs: data.rows
           }
-          console.log('tempV: ', templateVars)
-
       res.render('logged-in', templateVars)
     })
     .catch(err => {
@@ -29,21 +26,42 @@ module.exports = (db) => {
       .status(500)
       .json({ error: err.message });
       });
+  });
+
+
+  router.post('/', (req,res) => {
+    // const mapID =
+    const mapID = req.params.id;
+    console.log("fav add: ", mapID)
+    db.query(`INSERT INTO favourites (user_id, map_id)
+    VALUES (2, 2)
+    RETURNING *
+    ;`)
+
+    .then(data => {
+      const favMap = data.rows;
+      res.redirect('/favourite')
+    })
+
+  });
+
+  router.post('/:id/delete', (req,res) => {
+    const mapID = req.params.id
+    console.log('mapID delete: ', mapID)
+    db.query(`
+    DELETE
+    FROM favourites
+    WHERE map_id = ${mapID}
+    ;
+    `)
+
+    .then(data => {
+      const favMap = data.rows;
+      res.redirect('/favourite')
+    })
 
   });
 
   return router;
 };
 
-// %${username}%
-
-// router.get("/", (req, res) => {
-//   db.query(`SELECT * FROM maps;`)
-//   .then(data => {
-//     const username = req.session.user_id
-//     const templateVars = {
-//       username,
-//       maps: data.rows
-//     }
-//     res.render('maps', templateVars);
-//   })

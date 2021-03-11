ade5c77a5611d3dcd976830767b95175c21a8b2c
Molly Trepanier <mtrepanier123@gmail.com>
2021-03-11 00:26:04 +0000

    updated map to database to include lat/lon

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,4 +1,5 @@
 -- example seeds for maps table
 
-INSERT INTO maps (name, user_id) VALUES ('Forest Sightings', 1),
-('Urban Sightings', 1), ('Bigfoot Food Sources', 2);
+INSERT INTO maps (name, lon, lat, user_id)
+VALUES ('British Columbia', 50, -123.100, 1),
+('Ontario', 51.25, -85.32, 1), ('California',36.77, -119.41, 2);

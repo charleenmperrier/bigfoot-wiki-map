aab840630fd91187c207aea9927729e7e6c08633
Molly Trepanier <mtrepanier123@gmail.com>
2021-03-11 19:42:30 +0000

    images now render on pinss! movedb_photos to public/images && used express.static middleware so we host our own images

--- db/helpers/images.js
+++ db/helpers/images.js
@@ -1,44 +1,23 @@
-// const images = [
-//   "https://imgur.com/n6Tu8f9",
-//   "https://imgur.com/aGWptI6",
-//   "https://imgur.com/zwhl9HN",
-//   "https://imgur.com/jCe3F9G",
-//   "https://imgur.com/jcYSSPC",
-//   "https://imgur.com/kd2nBdC",
-//   "https://imgur.com/jPu76AS",
-//   "https://imgur.com/02kCZie",
-//   "https://imgur.com/28DUeie",
-//   "https://imgur.com/4tGGjnM",
-//   "https://imgur.com/7psUYd2",
-//   "https://imgur.com/DxQXJoI",
-//   "https://imgur.com/mgPP8PD",
-//   "https://imgur.com/l0GQvuB",
-//   "https://imgur.com/T9qbYRy",
-//   "https://imgur.com/wQOZ5hz",
-//   "https://imgur.com/Cko0Y6U",
-//   "https://imgur.com/Pkj22q1",
-//   "https://imgur.com/R4tBjnc"
-// ];
-//ask mentor how to get relative paths to images to load in pin-- it puts it in an img src ="path", but these pathes are
+
 const images = [
-  '/db_photos/bigfoot_1.jpeg',
-  '/db_photos/bigfoot_2.jpeg',
-  '/db_photos/bigfoot_3.jpeg',
-  '/db_photos/bigfoot_4.jpeg',
-  '/db_photos/bigfoot_5.jpeg',
-  '/db_photos/bigfoot_6.jpg',
-  '/db_photos/bigfoot_7.jpg',
-  '/db_photos/bigfoot_9.jpeg',
-  '/db_photos/bigfoot_10.jpeg',
-  '/db_photos/bigfoot_codes.jpeg',
-  '/db_photos/bigfoot_insurance.jpg',
-  '/db_photos/bigfoot_gardening.jpeg',
-  '/db_photos/bigfoot_interview.png',
-  '/db_photos/bigfoot_mask.jpeg',
-  '/db_photos/bigfoot_readings.jpeg',
-  '/db_photos/bigfoot_selfie.png',
-  '/db_photos/bigfoot_snacks.jpeg',
-  '/db_photos/bigfoot_ukulele.jpeg'
+  'http://localhost:8080/static/bigfoot_1.jpeg',
+  'http://localhost:8080/static/bigfoot_2.jpeg',
+  'http://localhost:8080/static/bigfoot_3.jpeg',
+  'http://localhost:8080/static/bigfoot_4.jpeg',
+  'http://localhost:8080/static/bigfoot_5.jpeg',
+  'http://localhost:8080/static/bigfoot_6.jpg',
+  'http://localhost:8080/static/bigfoot_7.jpg',
+  'http://localhost:8080/static/bigfoot_9.jpeg',
+  'http://localhost:8080/static/bigfoot_10.jpeg',
+  'http://localhost:8080/static/bigfoot_codes.jpeg',
+  'http://localhost:8080/static/bigfoot_insurance.jpg',
+  'http://localhost:8080/static/bigfoot_gardening.jpeg',
+  'http://localhost:8080/static/bigfoot_interview.png',
+  'http://localhost:8080/static/bigfoot_mask.jpeg',
+  'http://localhost:8080/static/bigfoot_readings.jpeg',
+  'http://localhost:8080/static/bigfoot_selfie.png',
+  'http://localhost:8080/static/bigfoot_snacks.jpeg',
+  'http://localhost:8080/static/bigfoot_ukulele.jpeg'
 ]
 
 module.exports = images

d9fdd6c42d783dfae65aadb2cac52e8592d3cd2b
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-11 03:21:34 +0000

    users marker location db pathing unconnected

--- public/scripts/user-map.js
+++ public/scripts/user-map.js
@@ -1,39 +1,61 @@
 // Map Element on page
-let map = L.map('map').setView([55, -101.2996], 4);
+
+let map = L.map('map', {
+  minZoom: 2,
+  maxZoom: 10,
+  worldCopyJump: true,
+
+}).setView([30, 0], 2);
+
+// dynamic constraint
+let bounds = map.getBounds().pad(0.65)
+map.setMaxBounds(bounds);
+
+// console.log(map.getBounds())
+
 const attribution = '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
 const tileUrl = 'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=wpKyMC536a0ZlXqAX7iu';
+
+
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(map);
-
 
 getData()
 
 // Toggle switch of Heart to Red Heart
-function myFunction(x) {
-  x.classList.toggle("fa-fa-heart");
+// function myFunction(x) {
+//   x.classList.toggle("fa-fa-heart");
+// }
+
+// zoom out on popup
+function zoomOut() {
+    map.flyTo([55, -101.2996], 4, {duration: 0.5}).closePopup();
 }
-
-
 
 
 // Map Marker, adds data from db to map marker popups
 async function getData() {
   const response = await fetch('/api/pins');
   const data = await response.json();
-  console.log(data.pins)
+  // console.log(data.pins)
   for (item of data.pins) {
     const marker = L.marker([item.lat, item.lon], {
       // hover description
       title: "big foot sighting",
     }).addTo(map);
 
-    let txt = `<h1> ${item.title} </h1> <div> <img src= ${'"'+item.picture_url+'"'} height="150px" width="auto"/> <p id="description"> ${item.description} </p> <p id="longLat">location at ${[(item.lat).toFixed(2), (item.lon).toFixed(2)]} </p></div> <i onclick="myFunction(this)" class="fa fa-heart"></i> `;
+    let txt = `<h1> ${item.title} </h1> <div> <img src= ${'"'+item.picture_url+'"'} height="150px" width="auto"/> <p id="description"> ${item.description} </p> <p id="longLat">location at ${[(item.lat).toFixed(2), (item.lon).toFixed(2)]} </p><button onclick="zoomOut()"> zoom out </button> `;
 
 
+// zoom in on popup
+    marker.on('click', function(e){
+      map.flyTo([e.latlng.lat, e.latlng.lng], 5, {duration: 0.5});
+
+    });
 
     marker.bindPopup(txt);
   }
-  console.log(data)
+  // console.log(data)
 }
 
 
@@ -52,6 +74,9 @@ async function getData() {
         };
 
       //Add a marker to show where you clicked.
-      theMarker = L.marker([lat,lon]).addTo(map).bindPopup(`<input id="title" type="text"/>  <div id="image-url"> <img src= "" height="150px" width="auto"/> <input id="description" type="text"/>  <p id="longLat">location at ${[(lat).toFixed(3), (lon).toFixed(3)]} </p></div>  <button type="submit" class="btn btn-primary">save location</button></form>`).openPopup();
+      theMarker = L.marker([lat,lon]).addTo(map).bindPopup(`<div id="marker-comment"> <input id="title" type="text" placeholder="Location"/> <input id="date" type="text" placeholder="Date"/>  <input id="img_url" type="text" placeholder="URL"/>  <input id="description" type="text" placeholder="Description"/>  <p id="longLat">location at ${[(lat).toFixed(3), (lon).toFixed(3)]} </p></div>  <button type="submit" class="btn btn-primary">save location</button></form> <button type="submit" class="btn btn-primary">delete location</button></form> </div> `).openPopup();
       });
 
+
+      // image source format
+      // <img src= ${'"'+item.picture_url+'"'} height="150px" width="auto"/>
